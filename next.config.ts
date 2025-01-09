3e74d899d023370b30ee8f0fe47314119d425865
AndriyCh1 <chekanovskiyacc@gmail.com>
2025-01-08 20:47:53 -0700

    Core setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  logging: { fetches: { fullUrl: true } }
 };
 
 export default nextConfig;
